458d5c7206fed3632afe753a8fc9dd46c4b6d487
limam dedi <dedilimam320@gamil.com>
2021-10-02 01:27:20 +0100

    my first project django

---
+++ bookstore/forms.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.forms import ModelForm
+
+from .models import Customer, Order
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class OrderForm(ModelForm):
+    class Meta:
+        model = Order
+        fields ="__all__"
+
+class CustomerForm(ModelForm):
+    class Meta:
+        model = Customer
+        fields ="__all__"
+        exclude =['user']
+
+class CreateNewUser(UserCreationForm):
+    class Meta:
+        model = User
+        fields =['username','email','password1','password2']
